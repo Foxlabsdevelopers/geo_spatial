a0511b076de04df7228892121adb1754a28b5f9a
Cresencio Vázquez Flores <cresenciofl@icloud.com>
2024-11-13 20:16:04 -0500

    Initialize map with leaflet

---
+++ assets/js/src/map.js
@@ -0,0 +1,40 @@
+const loadCss = (url, callback = () => {}) => {
+  const link = document.createElement("link");
+  link.href = url;
+  link.rel = "stylesheet";
+  link.type = "text/css";
+  link.onload = () => {
+    callback();
+  };
+
+  document.head.appendChild(link);
+};
+
+const loadScript = (url, callback = () => {}) => {
+  const script = document.createElement("script");
+  script.src = url;
+  script.type = "text/javascript";
+  script.async = true;
+  script.onload = () => {
+    callback();
+  };
+
+  document.head.appendChild(script);
+};
+
+const loadMap = () => {
+  const map = L.map("map").setView([20.683972, -87.064007], 18);
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+};
+
+const init = () => {
+  loadCss("https://unpkg.com/leaflet@1.9.4/dist/leaflet.css", () => {
+    loadScript("https://unpkg.com/leaflet@1.9.4/dist/leaflet.js", loadMap);
+  });
+};
+
+export { init };
